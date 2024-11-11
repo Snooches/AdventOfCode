7a2a2f6e00e5f16ee1e2880271ac31c3a7ca870d
Solysto <Solysto@pm.me>
2024-11-11 19:43:03 +0100

    Setup AoC 2018 Solve Day 1

---
+++ AdventOfCode/AoC2018/Day01/InputDataConverter.cs
@@ -0,0 +1,14 @@
+﻿using Utilities.Interfaces;
+
+namespace AoC2018.Day01;
+
+internal class InputDataConverter : IInputDataConverter<IEnumerable<int>>
+{
+	public IEnumerable<int> ConvertInputData(IFileReader fileReader)
+	{
+		foreach (string line in fileReader.ReadLines())
+		{
+			yield return int.Parse(line);
+		}
+	}
+}
---
+++ AdventOfCode/AoC2018/Day01/Solver.cs
@@ -0,0 +1,33 @@
+﻿using Utilities;
+using Utilities.Interfaces;
+
+namespace AoC2018.Day01;
+
+internal class Solver(IInputDataConverter<IEnumerable<int>> inputDataConverter, IFileReader fileReader)
+			 : AbstractSolver<IEnumerable<int>, long?, long?>(inputDataConverter, fileReader)
+{
+	protected override string SolutionTextA => $"The frequency after all shift is: {SolutionValueA}";
+	protected override string SolutionTextB => $"The first repeated frequency is: {SolutionValueB}";
+
+	protected override void SolveImplemented()
+	{
+		HashSet<long> visitedFrequencies = [0];
+		long currentFrequency = 0;
+		while (SolutionValueB is null)
+		{
+			foreach (int shift in inputData)
+			{
+				currentFrequency += shift;
+				if (!visitedFrequencies.Contains(currentFrequency))
+				{
+					visitedFrequencies.Add(currentFrequency);
+				}
+				else
+				{
+					SolutionValueB ??= currentFrequency;
+				}
+			}
+			SolutionValueA ??= currentFrequency;
+		}
+	}
+}
