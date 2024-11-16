5a34857ffebbaa198e7f47e02f20e6dbac349f67
Solysto <Solysto@pm.me>
2024-11-16 11:26:52 +0100

    Solve Day 5 Part 1

---
+++ AdventOfCode/AoC2018/Day05/InputDataConverter.cs
@@ -0,0 +1,11 @@
+﻿using Utilities.Interfaces;
+
+namespace AoC2018.Day05;
+
+internal class InputDataConverter : IInputDataConverter<string>
+{
+	public string ConvertInputData(IFileReader fileReader)
+	{
+		return fileReader.ReadLines().First();
+	}
+}
---
+++ AdventOfCode/AoC2018/Day05/Solver.cs
@@ -0,0 +1,34 @@
+﻿using Utilities;
+using Utilities.Interfaces;
+
+namespace AoC2018.Day05;
+
+internal class Solver(IInputDataConverter<string> inputDataConverter, IFileReader fileReader)
+			 : AbstractSolver<string, long?, long?>(inputDataConverter, fileReader)
+{
+	protected override string SolutionTextA => $"SolutionA is: {SolutionValueA}";
+	protected override string SolutionTextB => $"SolutionB is: {SolutionValueB}";
+
+	protected override void SolveImplemented()
+	{
+		SolutionValueA = Collapse(inputData).Length;
+	}
+
+	private static string Collapse(string polymer)
+	{
+		Stack<char> collapsedPolymer = new();
+		for (int i = 0; i < polymer.Length; i++)
+		{
+			if (collapsedPolymer.Count == 0
+				|| (collapsedPolymer.Peek() != polymer[i] + 32
+				  && collapsedPolymer.Peek() != polymer[i] - 32))
+			{
+				collapsedPolymer.Push(polymer[i]);
+				continue;
+			}
+			collapsedPolymer.Pop();
+
+		}
+		return new string(collapsedPolymer.ToArray());
+	}
+}
