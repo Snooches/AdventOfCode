79f5f12145ea1be6c8f14ebcfcd869ead1b225aa
Snooches <github.coconut503@passmail.net>
2024-12-02 09:47:51 +0100

    Complete Day 02 Part 2 feels inefficient but skipping first or second element makes determining ascending/descending complicated and it still terminates well before my 1 second threshold.

---
+++ AdventOfCode/AoC2024/Day02/InputDataConverter.cs
@@ -0,0 +1,14 @@
+﻿using Utilities.Interfaces;
+
+namespace AoC2024.Day02;
+
+internal class InputDataConverter : IInputDataConverter<IEnumerable<int[]>>
+{
+	public IEnumerable<int[]> ConvertInputData(IFileReader fileReader)
+	{
+		foreach (string line in fileReader.ReadLines())
+		{
+			yield return line.Split(' ').Select(x=>int.Parse(x)).ToArray();
+		}
+	}
+}
