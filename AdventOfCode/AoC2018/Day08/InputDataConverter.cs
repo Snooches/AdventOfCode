e31739bcf8e05b2c8546c13e52f24461887fbf5b
Snooches <github.coconut503@passmail.net>
2024-12-02 16:18:29 +0100

    Solve Day 08

---
+++ AdventOfCode/AoC2018/Day08/InputDataConverter.cs
@@ -0,0 +1,28 @@
+﻿using Utilities.Interfaces;
+
+namespace AoC2018.Day08;
+
+internal class InputDataConverter : IInputDataConverter<TreeNode>
+{
+	public TreeNode ConvertInputData(IFileReader fileReader)
+	{
+		List<int> input = fileReader.ReadLines().First().Split(' ').Select(int.Parse).ToList();
+		int currentIndex = 0;
+		return ReadNode(input, ref currentIndex);
+	}
+
+	private static TreeNode ReadNode(List<int> input, ref int currentIndex)
+	{
+		TreeNode node = new();
+		int childCount = input[currentIndex];
+		int dataCount = input[currentIndex + 1];
+		currentIndex += 2;
+		for (int i = 0; i < childCount; i++)
+		{
+			node.Children.Add(ReadNode(input, ref currentIndex));
+		}
+		node.Payload = input[currentIndex..(currentIndex + dataCount)];
+		currentIndex += dataCount;
+		return node;
+	}
+}
--- AdventOfCode/AoC2018/Day08/TreeNode.cs
+++ AdventOfCode/AoC2018/Day08/TreeNode.cs
@@ -2,6 +2,28 @@
 
 internal class TreeNode
 {
-	private HashSet<int> Payload { get; set; }
-	private HashSet<TreeNode> Children { get; set; }
+	public List<int> Payload { get; set; } = [];
+	public List<TreeNode> Children { get; set; } = [];
+
+	public int SumOfPayloads()
+	{
+		return Payload.Sum() + Children.Sum(x => x.SumOfPayloads());
+	}
+
+	public int NodeValue() 
+	{
+		if (Children.Count == 0)
+		{
+			return Payload.Sum();
+		}
+		int value = 0;
+		foreach(int index in Payload)
+		{
+			if(index > 0 && index <= Children.Count)
+			{
+				value += Children[index - 1].NodeValue();
+			}
+		}
+		return value;
+	}
 }
