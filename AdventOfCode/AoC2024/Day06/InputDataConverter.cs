56bd89f0e41c848ed275782ca9eecfe6ba01402a
Snooches <github.coconut503@passmail.net>
2024-12-07 08:43:30 +0100

    Solve Day 06

---
+++ AdventOfCode/AoC2024/Day06/InputDataConverter.cs
@@ -0,0 +1,31 @@
+﻿using Utilities.Interfaces;
+
+namespace AoC2024.Day06;
+
+internal class InputDataConverter : IInputDataConverter<((int,int),Dictionary<(int,int),State>)>
+{
+	public ((int,int),Dictionary<(int,int),State>) ConvertInputData(IFileReader fileReader)
+	{
+		Dictionary<(int, int), State> grid = [];
+		(int, int) startPosition = (0, 0);
+		int y = 0;
+		foreach (string line in fileReader.ReadLines())
+		{
+			int x = 0;
+			foreach(char c in line)
+			{
+				if (c == '^')
+					startPosition = (x, y);
+				grid.Add((x, y), c switch
+				{
+					'.' => State.Empty,
+					'#' => State.Blocked,
+					_ => State.Empty
+				});
+				x++;
+			}
+			y++;
+		}
+		return (startPosition, grid);
+	}
+}
---
+++ AdventOfCode/AoC2024/Day06/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024.Day06;
+public enum State
+{
+	Empty,
+	Blocked,
+	Visited
+}
