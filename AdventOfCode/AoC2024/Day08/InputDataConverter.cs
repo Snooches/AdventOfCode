5793c782b06bfc4dd014534e3c52a453f8360780
Snooches <github.coconut503@passmail.net>
2024-12-08 16:40:18 +0100

    Solve AoC 24 Day 08

---
+++ AdventOfCode/AoC2024/Day08/InputDataConverter.cs
@@ -0,0 +1,28 @@
+﻿using Utilities.Interfaces;
+
+namespace AoC2024.Day08;
+
+internal class InputDataConverter : IInputDataConverter<(IEnumerable<List<(int,int)>>,int,int)>
+{
+	public (IEnumerable<List<(int,int)>>,int,int) ConvertInputData(IFileReader fileReader)
+	{
+		Dictionary<char, List<(int, int)>> antennas = [];
+		int y = 0;
+		IEnumerable<string> lines = fileReader.ReadLines();
+		foreach (string line in lines)
+		{
+			int x = 0;
+			foreach (char space in line)
+			{
+				if (space != '.')
+					if (antennas.TryGetValue(space, out List<(int, int)>? positions))
+						positions.Add((x, y));
+					else
+						antennas[space] = [(x, y)];
+				x++;
+			}
+			y++;
+		}
+		return (antennas.Values, lines.First().Length-1, lines.Count()-1);
+	}
+}
---
+++ AdventOfCode/AoC2024/Day08/Solver.cs
@@ -0,0 +1,48 @@
+﻿using Utilities;
+using Utilities.Interfaces;
+
+namespace AoC2024.Day08;
+
+internal class Solver(IInputDataConverter<(IEnumerable<List<(int,int)>>,int,int)> inputDataConverter, IFileReader fileReader)
+			 : AbstractSolver<(IEnumerable<List<(int,int)>>,int,int), int?, long?>(inputDataConverter, fileReader)
+{
+	protected override string SolutionTextA => $"SolutionA is: {SolutionValueA}";
+	protected override string SolutionTextB => $"SolutionB is: {SolutionValueB}";
+
+	protected override void SolveImplemented()
+	{
+		HashSet<(int, int)> antinodesA = [];
+		HashSet<(int, int)> antinodesB = [];
+		foreach(List<(int X,int Y)> antennas in inputData.Item1)
+			for (int i = 0; i < antennas.Count -1; i++)
+				for (int j = i+1; j < antennas.Count; j++)
+				{
+					int xDif = antennas[i].X - antennas[j].X;
+					int yDif = antennas[i].Y - antennas[j].Y;
+					(int X, int Y) antinode = (antennas[i].X + xDif, antennas[i].Y + yDif);
+					if(antinode.X >= 0 && antinode.X <= inputData.Item2 && antinode.Y >= 0 && antinode.Y <= inputData.Item3)
+						antinodesA.Add(antinode);
+					antinode = (antennas[j].X - xDif, antennas[j].Y - yDif);
+					if (antinode.X >= 0 && antinode.X <= inputData.Item2 && antinode.Y >= 0 && antinode.Y <= inputData.Item3)
+						antinodesA.Add(antinode);
+					int multiplier = 1;
+					antinode = antennas[i];
+					while (antinode.X >= 0 && antinode.X <= inputData.Item2 && antinode.Y >= 0 && antinode.Y <= inputData.Item3)
+					{
+						antinodesB.Add(antinode);
+						antinode = (antennas[i].X + xDif * multiplier, antennas[i].Y + yDif * multiplier);
+						multiplier++;
+					}
+					multiplier = 1;
+					antinode = antennas[j];
+					while (antinode.X >= 0 && antinode.X <= inputData.Item2 && antinode.Y >= 0 && antinode.Y <= inputData.Item3)
+					{
+						antinodesB.Add(antinode);
+						antinode = (antennas[j].X - xDif * multiplier, antennas[j].Y - yDif * multiplier);
+						multiplier++;
+					}
+				}
+		SolutionValueA = antinodesA.Count;
+		SolutionValueB = antinodesB.Count;
+	}
+}
