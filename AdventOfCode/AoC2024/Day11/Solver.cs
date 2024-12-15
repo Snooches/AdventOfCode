0f56da476475189d5fae781b5a9e7cfb58c40248
Snooches <github.coconut503@passmail.net>
2024-12-15 18:34:34 +0100

    Not sure if this will ever work :(

--- AdventOfCode/AoC2024/Day11/Solver.cs
+++ AdventOfCode/AoC2024/Day11/Solver.cs
@@ -13,41 +13,15 @@ internal class Solver(IInputDataConverter<List<long>> inputDataConverter, IFileR
 	private readonly Dictionary<long, List<long>> calculatedValues = [];
 	protected override void SolveImplemented()
 	{
-		for (int i = 0; i < 75; i++)
-		{
-			Console.WriteLine($"After Round {i} - {calculatedValues.Count} calculated - {inputData.Count} Values in list");
-			Blink(inputData);
-		}
-		SolutionValueA = inputData.Count;
+		SolutionValueB = inputData.Sum(x => Blink(x, 25));
 	}
 
-	private void Blink(List<long> stones)
+	private long Blink(long start, int blinksLeft)
 	{
-		for (int i = stones.Count - 1; i >= 0; i--)
-		{
-			if (stones[i] == 0)
-			{
-				stones[i] = 1;
-				continue;
-			}
-			if (calculatedValues.TryGetValue(stones[i], out List<long>? nextValue))
-			{
-				stones[i] = nextValue[0];
-				if (nextValue.Count > 1)
-					stones.Insert(i + 1, nextValue[1]);
-				continue;
-			}
-			String stringValue = stones[i].ToString();
-			if (stringValue.Length % 2 == 0)
-			{
-				long key = stones[i];
-				stones[i] = long.Parse(stringValue[(stringValue.Length / 2)..]);
-				stones.Insert(i, long.Parse(stringValue[..(stringValue.Length / 2)]));
-				calculatedValues[key] = [stones[i], stones[i + 1]];
-				continue;
-			}
-			calculatedValues.Add(stones[i], [stones[i] * 2024]);
-			stones[i] = calculatedValues[stones[i]][0];
-		}
+		if (blinksLeft == 0)
+			return start;
+		if (calculatedValues.TryGetValue(start, out List<long>? nextValues) && nextValues.Count >= blinksLeft)
+			return nextValues[blinksLeft - 1];
+		long result = 
 	}
 }
